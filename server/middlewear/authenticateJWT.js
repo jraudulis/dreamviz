30299c0e3b43bd780647aa4ed0095cfd7cbd9093
Juris Raudulis <juris.raudulis@outlook.com>
2026-02-28 13:20:10 +0000

    Fixing some bugs

--- server/middlewear/authenticateJWT.js
+++ server/middlewear/authenticateJWT.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 const authenticateJWT = async (req, res, next) => {
     const authHeader = req.headers.authorization;
 
-    if(!authHeader) {
+    if(!authHeader || !authHeader.startsWith('Bearer')) {
         return res.status(401).json('Missing authorization header')
     }
 
@@ -13,10 +13,15 @@ const authenticateJWT = async (req, res, next) => {
         return res.status(401).json('Missing token');
     }
 
-    jwt.verify(token, process.env.JWT_SECRET, (err, payload) => {
-        if(err){
-            return res.status(403).json('invalid token');
-        }
+    jwt.verify(token, process.env.JWT_SECRET, async (err, payload) => {
+        if(err) return res.status(403).json('invalid token');
+
+        const user = await knex('users')
+            .where({id: payload.id})
+            .first();
+
+        if(!user) return res.status(403).json('user no longer exists')
+        
 
         req.user = payload;
         next();
